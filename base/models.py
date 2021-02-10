3d88e513b7764e5990a9c24cb6b9932811489625
pjy4183 <pjy4183@gmail.com>
2021-02-09 21:14:34 -0600

    Update posts and post page

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+class Post(models.Model):
+    headline = models.CharField(max_length=200)
+    sub_headline = models.CharField(max_length=200, null=True, blank=True)
+    #thumbnail =
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, null=True,)
+    #slug =
+
+    def __str__(self):
+        return self.headline
--- base/views.py
+++ base/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'base/index.html')
+    posts = Post.objects.filter(active=True,featured=True)[1:5]
+    context = {'posts':posts}
+    return render(request, 'base/index.html', context)
     
 def posts(request):
-    return render(request, 'base/posts.html')
+    posts = Post.objects.filter(active=True)
+    context = {'posts':posts}
+    return render(request, 'base/posts.html', context)
     
-def post(request):
-    return render(request, 'base/post.html')
+def post(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {'post': post}
+    return render(request, 'base/post.html', context)
     
 def profile(request):
     return render(request, 'base/profile.html')
