dc89e28fb30e9f18cdcc1548421d114a78f5c08c
pjy4183 <pjy4183@gmail.com>
2021-02-17 21:09:40 -0600

    ckeditor update

--- base/models.py
+++ base/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
@@ -11,7 +13,7 @@ class Post(models.Model):
     headline = models.CharField(max_length=200)
     sub_headline = models.CharField(max_length=200, null=True, blank=True)
     thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="placeholder.png")
-    body = models.TextField(null=True, blank=True)
+    body = RichTextUploadingField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
 
     'crispy_forms',
     'django_filters',
-
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -138,3 +139,20 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pjy4183@gmail.com'
+EMAIL_HOST_PASSWORD =''
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'full',
+        'height':300,
+        'width': '100%',
+    }
+}
